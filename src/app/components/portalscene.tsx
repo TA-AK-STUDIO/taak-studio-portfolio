3b8fdbc68e374412f474b3cf52356203315ea6bd
Pantera Claw <panteraclaw1@gmail.com>
2026-02-28 00:37:38 +0000

    fix: show Three.js scene in production (avoid z-index:-1 behind body)

--- src/app/components/portalscene.tsx
+++ src/app/components/portalscene.tsx
@@ -401,7 +401,8 @@ const UniverseScene: React.FC<UniverseSceneProps> = ({ zoom }) => {
         left: 0,
         width: "100%",
         height: "100%",
-        zIndex: -1,
+        zIndex: 0,
+        pointerEvents: "none",
       }}
     />
   );
